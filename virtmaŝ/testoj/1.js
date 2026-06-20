c438985978f6136292d08b2d88662c3b91631268
anonima programisto <programisto0@protonmail.com>
2026-06-20 19:19:51 +0300

    v0.1.3 Aldonitaj pli da komentoj en la testoj.

--- virtmaŝ/testoj/1.js
+++ virtmaŝ/testoj/1.js
@@ -1,3 +1,4 @@
+// Ni plenumas ununuran komandon
 const helpanto = require('./helpanto.cjs');
 helpanto.asertiLaEligonDeLaBajtkodo("testoj/1/1.idvm", Buffer.from([0x1, 0x31]));
 helpanto.asertiLaEligonDeLaProgramo("testoj/1/1.idvm", "1");
--- virtmaŝ/testoj/2.js
+++ virtmaŝ/testoj/2.js
@@ -1,3 +1,4 @@
+// Ni plenumas kelkajn komandojn
 const helpanto = require('./helpanto.cjs');
 helpanto.asertiLaEligonDeLaBajtkodo("testoj/2/2.idvm", Buffer.from([0x1, 0x31, 0x0, 0x1, 0x32]));
 helpanto.asertiLaEligonDeLaProgramo("testoj/2/2.idvm", "12");
